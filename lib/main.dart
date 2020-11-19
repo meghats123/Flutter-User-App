af3e58195b5690cbd1bf20b8b88b166da0343329
meghats123 <meghats39032@gmail.com>
2020-11-19 22:04:05 +0530

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:user_app/screens/user.dart';
 
 void main()=>runApp(MyApp());
 class MyApp extends StatelessWidget {
-  TextEditingController getUsername=TextEditingController();
-  TextEditingController getPassword=TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,49 +15,7 @@ class MyApp extends StatelessWidget {
           title: Text("User App"),
           backgroundColor: Colors.amber,
         ),
-        body: Container(
-          padding: EdgeInsets.all(10.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              TextField(
-                controller: getUsername,
-                decoration: InputDecoration(
-                  hintText: "Enter the Username",
-                  border: OutlineInputBorder()
-                ),
-              ),
-              SizedBox(height: 10.0,),
-              TextField(
-                controller: getPassword,
-                decoration: InputDecoration(
-                  hintText: "Enter the Password",
-                  border: OutlineInputBorder()
-                ),
-                obscureText: true,
-              ),
-              SizedBox(height: 10.0,),
-              RaisedButton(onPressed: (){
-                String getUname=getUsername.text;
-                String getPass=getPassword.text;
-                print(getUname);
-                print(getPass);
-                
-              },
-                color: Colors.red,
-                child: Text("LOGIN"),
-              ),
-              SizedBox(height: 10.0,),
-              RaisedButton(onPressed: (){
-                
-              },
-                color: Colors.red,
-                child: Text("REGISTER"),
-              )
-            ],
-          ),
-        ),
+        body: CheckApp(),
       ),
     );
   }
---
+++ lib/screens/user.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+
+class CheckApp extends StatefulWidget {
+  @override
+  _CheckAppState createState() => _CheckAppState();
+}
+
+class _CheckAppState extends State<CheckApp> {
+  TextEditingController getUsername=TextEditingController();
+  TextEditingController getPassword=TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          TextField(
+            controller: getUsername,
+            decoration: InputDecoration(
+                hintText: "Enter the Username",
+                border: OutlineInputBorder()
+            ),
+          ),
+          SizedBox(height: 10.0,),
+          TextField(
+            controller: getPassword,
+            decoration: InputDecoration(
+                hintText: "Enter the Password",
+                border: OutlineInputBorder()
+            ),
+            obscureText: true,
+          ),
+          SizedBox(height: 10.0,),
+          RaisedButton(onPressed: (){
+            String getUname=getUsername.text;
+            String getPass=getPassword.text;
+            print(getUname);
+            print(getPass);
+
+          },
+            color: Colors.red,
+            child: Text("LOGIN"),
+          ),
+          SizedBox(height: 10.0,),
+          RaisedButton(onPressed: (){
+
+          },
+            color: Colors.red,
+            child: Text("REGISTER"),
+          )
+        ],
+      ),
+    );
+  }
+}
